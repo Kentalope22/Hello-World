47fa90b7807f3c771eece84f76495a69a706d93a
Kentalope22 <69648019+Kentalope22@users.noreply.github.com>
2020-08-13 11:45:51 -0700

    Add files via upload

---
+++ hello_world.java
@@ -0,0 +1,16 @@
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			System.out.println("Hello World");
+			System.out.println("My hobbies are gaming and reading");
+			System.out.println("My current favorite game of the year is Ghost of Tsushima");
+			System.out.println("A sport that I did before quarantine is Dragon Boat");
+			System.out.println("Some majors I am considering are Computer Science or 3D Modeling");
+			System.out.println("A college I am thinking of is SJSU");
+			
+	}	
+			
+}
